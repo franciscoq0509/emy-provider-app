dcd340d799392ce0a0b07f2639aa0e407ac04b4d
craigmark <cisher.m@gmail.com>
2017-11-21 11:04:01 +1300

    new actions added and edited

--- src/actions/customers.js
+++ src/actions/customers.js
@@ -1,27 +1,57 @@
+export const invalidateCustomers = () => ({ //user refreshes
+    type: 'INVALIDATE_CUSTOMERS'
+});
 
-export const addCustomerChunk = (newChunk) => ({
+export const requestCustomers = () => ({ //fetch new customers
+    type: 'REQUEST_CUSTOMERS'
+});
+
+export const receiveNewCustomers = (newCustomers) => ({ //recieving new customers success
+    type: 'RECEIVE_CUSTOMERS_SUCCESS',
+    allCustomers : newCustomers.results,
+    receivedAt: Date.now()
+});
+
+export const receiveCustomersError = (error) => ({ //recieving new customers failure
+    type: 'RECEIVE_CUSTOMERS_FAILURE',
+    error,
+    receivedAt: Date.now()
+});
+
+export const addCustomerChunk = (newChunk) => ({ //save Customers
     type: 'SAVE_CHUNK',
     allCustomers : newChunk.results
 });
 
-export const saveCustomerDetails = (customerDetails) => ({
-    type: 'SAVE_CUSTOMER_DETAILS',
-    customerDetails
-});
 
-export const selectCustomerForDetails = (id) => ({
+
+
+
+
+export const selectCustomerForDetails = (id) => ({ //user clicks on customers name
     type: 'SELECT_CUSTOMER_FOR_DETAILS',
     id
 });
 
-export const invalidateCustomers = () => ({
-    type: 'INVALIDATE_CUSTOMERS'
+export const requestCustomerDetails = (id) => ({ //fetches customer details
+    type: 'REQUEST_CUSTOMER_DETAILS',
+    id
 });
 
-export const receiveNewCustomers = (newCustomers) => ({
-    type: 'RECEIVE_CUSTOMERS',
-    allCustomers : newCustomers.results,
-    receivedAt: Date.now()
+export const recieveCustomerDetailsSuccess = (id) => ({ //recieve new customer details success
+    type: 'RECIEVE_CUSTOMER_DETAILS_SUCCESS',
+    id
+});
+
+export const recieveCustomerDetailsFailure = (error) => ({ //recieve new customer details failure
+    type: 'RECIEVE_CUSTOMER_DETAILS_FAILURE',
+    error
+});
+
+
+export const saveCustomerDetails = (customerDetails) => ({ //save customer details
+    type: 'SAVE_CUSTOMER_DETAILS',
+    customerDetails
 });
 
 //create a new action creator that handles request failures
