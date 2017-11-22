fd4b318f5c107417eb878e988f4ce6412a7bcfd3
craigmark <cisher.m@gmail.com>
2017-11-22 16:13:46 +1300

    list now coloured to distinguish between parents and children

--- src/components/CustomerItem.js
+++ src/components/CustomerItem.js
@@ -6,6 +6,7 @@ import { addNavigationHelpers } from 'react-navigation';
 export const CustomerItem = (props) => {
     return (
         <ListItem
+        containerStyle={props.item.is_child ? {} : {backgroundColor: '#e6f4f4'}}
             roundAvatar
             title={`${props.item.first_name} ${props.item.last_name}`}
             onPress={() => {props.item.customNavigator.navigate('fullDetail', { customerId : props.item.registered })}}
