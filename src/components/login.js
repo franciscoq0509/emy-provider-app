b8d7ea870a4ae71a74a5d8b2442194778b817a37
craigmark <cisher.m@gmail.com>
2017-11-28 15:06:25 +1300

    work on JWT auth

--- src/components/login.js
+++ src/components/login.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import Header from './Header';
 import {FormLabel, FormInput} from 'react-native-elements';
+import { createAuth } from '../utilities/createAuth';
 
 
 const inputChange = (text) => {
     console.log(text);
+    createAuth('jerrys@gymowner.cxm', 'L#N#marlin28');
 }
 
 export const login = () => (
--- src/utilities/createAuth.js
+++ src/utilities/createAuth.js
@@ -2,7 +2,7 @@ const Buffer = require('buffer/').Buffer;
 
 const _options = (guid, uname, pwd) => ({
     method: 'GET',
-    uri: 'http://login-dev.enrolmy.com/login?scope=users,client-users,addresses,phones,ethnic-groups,emergency-contacts,family-doctors,educations',
+    uri: 'https://login-dev.enrolmy.com/login?scope=users,client-users,addresses,phones,ethnic-groups,emergency-contacts,family-doctors,educations',
     headers: {
         'Authorization' : `Basic ${new Buffer(uname + ':' + pwd).toString('base64')}`,
         'X-enrolmy-slug' : guid
@@ -10,9 +10,13 @@ const _options = (guid, uname, pwd) => ({
 });
 
 export const createAuth = (uname, pwd, guid = '55790419-dbb4-43b4-9c1d-7bae0a37004f') => {
-    console.log(_options(uname, pwd, guid));
+    console.log(_options(guid, uname, pwd));
     fetch('https://login-dev.enrolmy.com/login?scope=users,client-users,addresses,phones,ethnic-groups,emergency-contacts,family-doctors,educations',_options(guid, uname, pwd))
-        .then((data) => {
-            console.log(data);
-        })
-}
+        .then(
+            (data) => {
+                console.log(data);
+            },
+            (error) => {console.log(eror)}
+        )
+        .catch((err) => console.log(err));
+}   
