53b6f40713c99912a93c4b4b70730e7b6b748c70
craigmark <cisher.m@gmail.com>
2018-01-26 10:41:58 +1300

    WIP

--- src/utilities/customerDetailsLoadingInit.js
+++ src/utilities/customerDetailsLoadingInit.js
@@ -2,6 +2,6 @@ let rp = require('request-promise');
 
 export const initReqObject = (id) => ({
     id,
-    sendReq: (options) => rp(options),
+    sendReq: options => rp(options),
     allowedToFetch: false
 });
